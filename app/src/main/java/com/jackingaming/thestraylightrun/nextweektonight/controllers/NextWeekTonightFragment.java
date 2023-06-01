b36ad8ee2f1b0e6a3664f73b9d0d11ad7c6afd19
jamesgov <blindmice1o3@gmail.com>
2023-05-31 20:04:05 -0700

    feat: Connect hardware camera to NextWeekTonightFragment's SurfaceView
    A SurfaceView is stacked on top of AnimatedLogoView. The SurfaceView is filling the right 3/4 of the screen (showing 1/4 of the AnimatedLogoView on the left side of the screen). It is connected to the hardware camera displays the camera's preview using its SurfaceHolder.

--- app/src/main/java/com/jackingaming/thestraylightrun/nextweektonight/controllers/NextWeekTonightFragment.java
+++ app/src/main/java/com/jackingaming/thestraylightrun/nextweektonight/controllers/NextWeekTonightFragment.java
@@ -1,6 +1,7 @@
 package com.jackingaming.thestraylightrun.nextweektonight.controllers;
 
 import android.animation.ObjectAnimator;
+import android.hardware.Camera;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,13 +10,17 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.jackingaming.thestraylightrun.R;
 import com.jackingaming.thestraylightrun.nextweektonight.views.AnimatedLogoView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class NextWeekTonightFragment extends Fragment {
     public static final String TAG = NextWeekTonightFragment.class.getSimpleName();
@@ -27,6 +32,8 @@ public class NextWeekTonightFragment extends Fragment {
     private String mParam2;
 
     private AnimatedLogoView animatedLogoView;
+    private SurfaceView surfaceView;
+    private Camera camera;
 
     public NextWeekTonightFragment() {
         // Required empty public constructor
@@ -62,6 +69,51 @@ public class NextWeekTonightFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Log.i(TAG, "onViewCreated()");
+        surfaceView = view.findViewById(R.id.surface_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                // Tell the camera to use this surface as its preview area
+                try {
+                    if (camera != null) {
+                        camera.setPreviewDisplay(surfaceHolder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+                if (camera == null) {
+                    return;
+                }
+
+                // The surface has changed size; update the camera preview size
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), i1, i2);
+                parameters.setPreviewSize(s.width, s.height);
+                camera.setParameters(parameters);
+                try {
+                    camera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    camera.release();
+                    camera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                // We can no longer display on this surface, so stop the preview.
+                if (camera != null) {
+                    camera.stopPreview();
+                }
+            }
+        });
+
         animatedLogoView = view.findViewById(R.id.animated_logo_view);
         ObjectAnimator animation = ObjectAnimator.ofFloat(animatedLogoView, "xLogo", 800f);
         animation.setInterpolator(new AccelerateDecelerateInterpolator());
@@ -69,5 +121,53 @@ public class NextWeekTonightFragment extends Fragment {
         animation.setRepeatCount(-1);
         animation.setRepeatMode(ObjectAnimator.REVERSE);
         animation.start();
+    }
+
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, int width, int height) {
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Camera.Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+        camera = Camera.open(0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
     }
 }
