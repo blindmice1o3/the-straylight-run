079dc77a6539a67360045c07d89d9d72a58532e4
jamesgov <blindmice1o3@gmail.com>
2024-01-29 18:17:16 -0800

    tinker: Add oat milk

--- app/src/main/java/com/jackingaming/thestraylightrun/sequencetrainer/hotbar/refrigerator/RefrigeratorFragment.java
+++ app/src/main/java/com/jackingaming/thestraylightrun/sequencetrainer/hotbar/refrigerator/RefrigeratorFragment.java
@@ -19,13 +19,14 @@ public class RefrigeratorFragment extends Fragment {
 
     public static final String TAG_MILK_TWO_PERCENT = "twoPercent";
     public static final String TAG_MILK_WHOLE = "whole";
+    public static final String TAG_MILK_OAT = "oat";
     public static final String TAG_MILK_COCONUT = "coconut";
     public static final String TAG_MILK_ALMOND = "almond";
     public static final String TAG_MILK_SOY = "soy";
 
     private ConstraintLayout clRefrigerator;
     private Milk milkTwoPercent, milkWhole;
-    private Milk milkCoconut, milkAlmond, milkSoy;
+    private Milk milkOat, milkCoconut, milkAlmond, milkSoy;
 
     public static RefrigeratorFragment newInstance() {
         Log.e(TAG, "newInstance()");
@@ -48,12 +49,14 @@ public class RefrigeratorFragment extends Fragment {
 
         milkTwoPercent = view.findViewById(R.id.milk_two_percent);
         milkWhole = view.findViewById(R.id.milk_whole);
+        milkOat = view.findViewById(R.id.milk_oat);
         milkCoconut = view.findViewById(R.id.milk_coconut);
         milkAlmond = view.findViewById(R.id.milk_almond);
         milkSoy = view.findViewById(R.id.milk_soy);
 
         milkTwoPercent.setTag(TAG_MILK_TWO_PERCENT);
         milkWhole.setTag(TAG_MILK_WHOLE);
+        milkOat.setTag(TAG_MILK_OAT);
         milkCoconut.setTag(TAG_MILK_COCONUT);
         milkAlmond.setTag(TAG_MILK_ALMOND);
         milkSoy.setTag(TAG_MILK_SOY);
