11052d56f30a6803627a947eeb5d58905e75332d
jamesgov <blindmice1o3@gmail.com>
2026-01-12 17:06:26 -0800

    tinker: Update image of items that are subclass of Seed

--- app/src/main/java/com/jackingaming/thestraylightrun/accelerometer/game/gameconsole/game/scenes/items/seeds/Seed.java
+++ app/src/main/java/com/jackingaming/thestraylightrun/accelerometer/game/gameconsole/game/scenes/items/seeds/Seed.java
@@ -2,6 +2,9 @@ package com.jackingaming.thestraylightrun.accelerometer.game.gameconsole.game.sc
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import com.jackingaming.thestraylightrun.R;
 import com.jackingaming.thestraylightrun.accelerometer.game.gameconsole.game.Game;
@@ -22,12 +25,28 @@ public abstract class Seed extends Item
 
         tileCommand = new SeedGrowableTileCommand(null, name);
         tileCommand.init(game);
+
+        Paint paintText = new Paint();
+        paintText.setColor(Color.RED);
+        paintText.setTextSize(72);
+        paintText.setAntiAlias(true);
+        Bitmap itemSpriteAndText = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(itemSpriteAndText);
+        canvas.drawBitmap(image, 0, 0, paintText);
+        canvas.drawText(name,
+                (int) (image.getWidth() * 0.17),
+                (int) (image.getHeight() * 0.42),
+                paintText);
+
+        image = itemSpriteAndText;
     }
 
     @Override
     protected void initImage() {
-        Bitmap spriteSheet = BitmapFactory.decodeResource(game.getContext().getResources(), R.drawable.items_and_tiles);
-        image = Bitmap.createBitmap(spriteSheet, 717, 363, 242, 272);
+        image = BitmapFactory.decodeResource(game.getContext().getResources(), R.drawable.item_seeds);
+//        Bitmap spriteSheet = BitmapFactory.decodeResource(game.getContext().getResources(), R.drawable.items_and_tiles);
+//        image = Bitmap.createBitmap(spriteSheet, 717, 363, 242, 272);
+
     }
 
     @Override
